bd853a4a19809cd7233b5ec84b16ee71bb5226b4
Richard Rosario <richardrosariojr@gmail.com>
2019-03-11 23:14:03 -0400

    added models and learned migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length = 100) #a character field of our post table 
+	content = models.TextField()
+	date_posted = models.DateTimeField(default =timezone.now)
+	author = models.ForeignKey(User, on_delete = models.CASCADE) #if a user is deleted, delete his posts 
+
+	def __str__(self):
+		return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 #from django.http import HttpResponse -> if no templates used, use this in addition
 
 
-posts = [
-	{
-		'author': 'Richard',
-		'title': 'My first post',
-		'content': 'First post content',
-		'date_posted': '3/11/19'
-
-	},
-
-	{
-		'author': 'Chisca',
-		'title': 'My second post',
-		'content': 'Second post content',
-		'date_posted': '3/13/19'
-
-	}
-
-]
 
 
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request,'blog/home.html',context) #adding third arg makes post dictionary accesible  in template
 
